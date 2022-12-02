7e5b3f19baaad0fda00e60466fce99bd9f530e6f
Collins U <collinsushi@gmail.com>
2022-12-02 01:51:51 +0000

    Init project

---
+++ server/app.js
@@ -0,0 +1,16 @@
+const express = require('express');
+//This express-graphql module  allows express to understand GraphQL & provides a simple way to create an express server that runs the graphQL api
+const { graphqlHTTP } = require('express-graphql'); 
+
+const app = express()
+
+app.use('/graphql', graphqlHTTP({ 
+    // schema: MyGraphQLSchema, 
+    // graphiql: true 
+}),
+);
+
+app.listen(4600, ()=>{
+    console.log('GraphQL Dev ✈️ Server running on PORT: 4600');
+})
+
