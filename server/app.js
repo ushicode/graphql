77c4cb0b252d4b10cb58c1bf499b5827299e2b03
Collins U <collinsushi@gmail.com>
2022-12-03 06:59:29 +0000

    Add book schema

--- server/app.js
+++ server/app.js
@@ -1,12 +1,14 @@
 const express = require('express');
 //This express-graphql module  allows express to understand GraphQL & provides a simple way to create an express server that runs the graphQL api
 const { graphqlHTTP } = require('express-graphql'); 
+const schema = require('./schema/schema')
+
 
 const app = express()
 
 app.use('/graphql', graphqlHTTP({ 
-    // schema: MyGraphQLSchema, 
-    // graphiql: true 
+    schema,
+    graphiql: true
 }),
 );
 
