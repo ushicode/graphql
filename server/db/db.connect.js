0e9140102614b658c9c25eb9545f4efe1bf2f5c5
Collins U <collinsushi@gmail.com>
2023-01-07 00:55:36 +0000

    debug

--- server/db/db.connect.js
+++ server/db/db.connect.js
@@ -9,7 +9,7 @@ const connect = async () => {
       {useNewUrlParser: true }
       );
   } catch (err) {
-    console.log(`CONNECTION ERR : ${err.message}`);
+    console.log(`SERVER CONNECTION ERR : ${err.message}`);
   }
 };
 
